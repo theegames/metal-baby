cc1950e8c25d186750c89122d7c9cb0b95e79f03
Mithil Verma <mithilproof@Mithils-MacBook-Pro.local>
2020-10-01 02:09:04 +0530

    Updates

---
+++ precache-manifest.2664b3d2cf9b476f490463f38cf01f62.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "246c42e45a16ab4ca706",
+    "url": "/metal-baby/static/css/main.26b5f586.chunk.css"
+  },
+  {
+    "revision": "246c42e45a16ab4ca706",
+    "url": "/metal-baby/static/js/main.246c42e4.chunk.js"
+  },
+  {
+    "revision": "b53bc062d6a7b6284ce6",
+    "url": "/metal-baby/static/js/1.b53bc062.chunk.js"
+  },
+  {
+    "revision": "d4b8474f49d8b6b618b3",
+    "url": "/metal-baby/static/js/runtime~main.d4b8474f.js"
+  },
+  {
+    "revision": "8792f04ffeb04b7eb6e41e1ec3edbe8a",
+    "url": "/metal-baby/static/media/robot-eating.8792f04f.png"
+  },
+  {
+    "revision": "ad90aa17f12771983a17f03d975b2b0a",
+    "url": "/metal-baby/static/media/robot-bathing.ad90aa17.png"
+  },
+  {
+    "revision": "1e419e0102754202cbb70e12347c3e8e",
+    "url": "/metal-baby/static/media/robot-playing.1e419e01.png"
+  },
+  {
+    "revision": "322ed1264b6909a8df7e8e455f25eea1",
+    "url": "/metal-baby/static/media/robot-idle.322ed126.png"
+  },
+  {
+    "revision": "bde8a4d14bd9cfe6f4f24f9b3cd3bf4e",
+    "url": "/metal-baby/index.html"
+  }
+];
